6a1c296eb7f02652f6e210ccbe428dd9ebfc2ae6
marshtv <marshtv.1972@gmail.com>
2022-08-27 13:05:30 +0300

    Complete

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <cassert>
+
+float travelTime(float distance, float speed) {
+	assert(speed > 0);
+	return distance / speed;
+}
 
 int main() {
-	
+	float distance, speed;
+	std::cout 	<< "You need to know travel time?" << std::endl
+				<< "Please, input following parameters:" << std::endl
+				<< "distance (km):";
+	std::cin >> distance;
+	std::cout << "speed (A positive number greater than zero) (km/h):";
+	std::cin >> speed;
+
+	int travelInMinutes = 60 * travelTime(distance, speed);
+	int hours = travelInMinutes / 60;
+	int minutes = travelInMinutes % 60;
+
+	std::cout << "____________________" << std::endl;
+	std::cout << "Travel Time is " << hours << "h " << minutes << "min." << std::endl;
 }
